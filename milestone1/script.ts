25733ef6719a38d0ef0e6c2a3812b6fa2f5fd704
Muhammad Ibrahim <160228904+Ibrahimnazee@users.noreply.github.com>
2024-09-14 14:30:14 +0500

    Add files via upload

---
+++ milestone1/script.ts
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementById('toggle-skills') as HTMLButtonElement
+const skill = document.getElementById('skill') as HTMLButtonElement
+
+toggleButton.addEventListener('click',()=>{
+    if (skill.style.display === 'none'){
+        skill.style.display = 'block'
+    }else {
+        skill.style.display ='none'
+    }
+});
